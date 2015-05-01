36d15ff9d8bb45784208e3db82152f4f5b11ad0d
harishbisht8 <harish.bisht@comprotechnologies.com>
2015-05-01 18:34:53 +0530

    second commit

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,150 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config(['$stateProvider',
+            '$urlRouterProvider',
+            function ($stateProvider,
+    $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+            }]);
+app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
+  $scope.test = 'flapperNews';
+  $scope.posts = posts.posts;
+  $scope.addPost = function () {
+    if (!$scope.title || $scope.title === '' || !$scope.link || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: []
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function (post) {
+    post.upvotes++;
+  };
+
+}]);
+
+app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', function ($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if (!$scope.body || $scope.body === '' || !$scope.author || $scope.author === '') {
+      return;
+    }
+    $scope.post.comments.push( {
+            author: $scope.author,
+            body: $scope.body,
+            upvotes: 0
+          });
+    $scope.body='';
+  };
+
+  $scope.incrementUpvotes = function (comment) {
+    comment.upvotes++;
+  };
+}])
+
+app.factory('posts', [function () {
+  var o = {
+    posts: [
+      {
+        title: 'post 1',
+        upvotes: 5,
+        comments: [
+          {
+            author: 'Joe',
+            body: 'Cool post!',
+            upvotes: 0
+          },
+          {
+            author: 'Bob',
+            body: 'Great idea but everything is wrong!',
+            upvotes: 0
+          }
+  ]
+    },
+      {
+        title: 'post 2',
+        upvotes: 2,
+        comments: [
+          {
+            author: 'Joe',
+            body: 'Cool post!',
+            upvotes: 0
+          },
+          {
+            author: 'Bob',
+            body: 'Great idea but everything is wrong!',
+            upvotes: 0
+          }
+  ]
+    },
+      {
+        title: 'post 3',
+        upvotes: 15,
+        comments: [
+          {
+            author: 'Joe',
+            body: 'Cool post!',
+            upvotes: 0
+          },
+          {
+            author: 'Bob',
+            body: 'Great idea but everything is wrong!',
+            upvotes: 0
+          }
+  ]
+    },
+      {
+        title: 'post 4',
+        upvotes: 9,
+        comments: [
+          {
+            author: 'Joe',
+            body: 'Cool post!',
+            upvotes: 0
+          },
+          {
+            author: 'Bob',
+            body: 'Great idea but everything is wrong!',
+            upvotes: 0
+          }
+  ]
+    },
+      {
+        title: 'post 5',
+        upvotes: 4,
+        comments: [
+          {
+            author: 'Joe',
+            body: 'Cool post!',
+            upvotes: 0
+          },
+          {
+            author: 'Bob',
+            body: 'Great idea but everything is wrong!',
+            upvotes: 0
+          }
+  ]
+    }
+                ]
+  };
+  return o;
+}]);
