83a76427b8a4b1268125e8b843084c0a471aae7c
harishbisht8 <harish.bisht@comprotechnologies.com>
2015-05-05 19:05:07 +0530

    .

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -8,7 +8,12 @@ app.config(['$stateProvider',
       .state('home', {
         url: '/home',
         templateUrl: '/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts', function (posts) {
+            return posts.getAll();
+          }]
+        }
       })
       .state('posts', {
         url: '/posts/{id}',
@@ -44,16 +49,16 @@ app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
 app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', function ($scope, $stateParams, posts) {
   $scope.post = posts.posts[$stateParams.id];
 
-  $scope.addComment = function() {
+  $scope.addComment = function () {
     if (!$scope.body || $scope.body === '' || !$scope.author || $scope.author === '') {
       return;
     }
-    $scope.post.comments.push( {
-            author: $scope.author,
-            body: $scope.body,
-            upvotes: 0
-          });
-    $scope.body='';
+    $scope.post.comments.push({
+      author: $scope.author,
+      body: $scope.body,
+      upvotes: 0
+    });
+    $scope.body = '';
   };
 
   $scope.incrementUpvotes = function (comment) {
@@ -62,94 +67,94 @@ app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', function ($scope,
 }])
 
 app.factory('posts', ['$http', function ($http) {
-  var o={};
+  var o = {};
 
-  o.getAll = function() {
-    return $http.get('/posts').success(function(data) {
-      angular.copy(data, o.posts)
-    })
-  }
-  /*o.posts = [
-      {
-        title: 'post 1',
-        upvotes: 5,
-        comments: [
-          {
-            author: 'Joe',
-            body: 'Cool post!',
-            upvotes: 0
-          },
-          {
-            author: 'Bob',
-            body: 'Great idea but everything is wrong!',
-            upvotes: 0
-          }
-  ]
-    },
-      {
-        title: 'post 2',
-        upvotes: 2,
-        comments: [
-          {
-            author: 'Joe',
-            body: 'Cool post!',
-            upvotes: 0
-          },
-          {
-            author: 'Bob',
-            body: 'Great idea but everything is wrong!',
-            upvotes: 0
-          }
-  ]
-    },
-      {
-        title: 'post 3',
-        upvotes: 15,
-        comments: [
-          {
-            author: 'Joe',
-            body: 'Cool post!',
-            upvotes: 0
-          },
-          {
-            author: 'Bob',
-            body: 'Great idea but everything is wrong!',
-            upvotes: 0
-          }
-  ]
-    },
-      {
-        title: 'post 4',
-        upvotes: 9,
-        comments: [
-          {
-            author: 'Joe',
-            body: 'Cool post!',
-            upvotes: 0
-          },
-          {
-            author: 'Bob',
-            body: 'Great idea but everything is wrong!',
-            upvotes: 0
-          }
-  ]
-    },
-      {
-        title: 'post 5',
-        upvotes: 4,
-        comments: [
-          {
-            author: 'Joe',
-            body: 'Cool post!',
-            upvotes: 0
-          },
-          {
-            author: 'Bob',
-            body: 'Great idea but everything is wrong!',
-            upvotes: 0
-          }
-  ]
+  o.getAll = function () {
+      return $http.get('/posts').success(function (data) {
+        angular.copy(data, o.posts)
+      })
     }
-                ];*/
+    /*o.posts = [
+        {
+          title: 'post 1',
+          upvotes: 5,
+          comments: [
+            {
+              author: 'Joe',
+              body: 'Cool post!',
+              upvotes: 0
+            },
+            {
+              author: 'Bob',
+              body: 'Great idea but everything is wrong!',
+              upvotes: 0
+            }
+    ]
+      },
+        {
+          title: 'post 2',
+          upvotes: 2,
+          comments: [
+            {
+              author: 'Joe',
+              body: 'Cool post!',
+              upvotes: 0
+            },
+            {
+              author: 'Bob',
+              body: 'Great idea but everything is wrong!',
+              upvotes: 0
+            }
+    ]
+      },
+        {
+          title: 'post 3',
+          upvotes: 15,
+          comments: [
+            {
+              author: 'Joe',
+              body: 'Cool post!',
+              upvotes: 0
+            },
+            {
+              author: 'Bob',
+              body: 'Great idea but everything is wrong!',
+              upvotes: 0
+            }
+    ]
+      },
+        {
+          title: 'post 4',
+          upvotes: 9,
+          comments: [
+            {
+              author: 'Joe',
+              body: 'Cool post!',
+              upvotes: 0
+            },
+            {
+              author: 'Bob',
+              body: 'Great idea but everything is wrong!',
+              upvotes: 0
+            }
+    ]
+      },
+        {
+          title: 'post 5',
+          upvotes: 4,
+          comments: [
+            {
+              author: 'Joe',
+              body: 'Cool post!',
+              upvotes: 0
+            },
+            {
+              author: 'Bob',
+              body: 'Great idea but everything is wrong!',
+              upvotes: 0
+            }
+    ]
+      }
+                  ];*/
   return o;
 }]);
